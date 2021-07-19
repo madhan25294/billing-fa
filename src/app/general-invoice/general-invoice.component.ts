c072b16992cc6b0cdd27e9a192702145eb400fc9
chaitanya-gaian <geddada.chaitanya@gaiansolutions.com>
2021-07-19 08:38:23 +0530

    added editable grid

--- src/app/general-invoice/general-invoice.component.ts
+++ src/app/general-invoice/general-invoice.component.ts
@@ -8,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class GeneralInvoiceComponent implements OnInit {
   gridData: any;
   constructor() { 
-    this.gridData = Array.from(Array(100).keys());
+    // this.gridData = Array.from(Array(100).keys());
+    this.gridData = [];
+    this.initGridData();
+  }
+
+  initGridData() {
+   for (let i = 0; i < 100; i++) {
+    this.gridData.push({
+      Line: i,
+      GL: i,
+      Text: i,
+      Cost: i,
+      Qty: i,
+      Rate: i,
+      Ext: i,
+      GroupHead: i,
+      GroupID: i,
+      CreditMemo: i
+    })
+   }
   }
 
   ngOnInit(): void {
